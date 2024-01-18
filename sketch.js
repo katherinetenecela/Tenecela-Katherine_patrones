9713bce25a3057bef9d3f56c64d8d951e2159b23
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2024-01-18 13:40:07 -0500

    numeros 0101

--- sketch.js
+++ sketch.js
@@ -1,3 +1,123 @@
+const celdas = [];
+const RETICULA = [];
+
+const azulejos = [];
+const NA = 16;
+const reglas = [
+  {
+    //Tile 0
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //Tile 1
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //Tile 2
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //Tile 3
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //Tile 4
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //Tile 5
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {
+    //Tile 6
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //Tile 7
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //Tile 8
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //Tile 9
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //Tile 10
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //Tile 11
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //Tile 12
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //Tile 13
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //Tile 14
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {
+    //Tile 15
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
